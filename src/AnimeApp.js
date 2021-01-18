4cc81519af792fc662b3fa6e13e145b82a395212
chriszdeux <chriszdeux@gmail.com>
2021-01-18 12:55:34 -0600

    starting

---
+++ src/AnimeApp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const AnimeApp = () => {
+  return (
+    <>
+      <h1>AnimeApp</h1>
+    </>
+  )
+}
---
+++ src/components/header/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Menu = () => {
+  return (
+    <header>
+      <nav>
+        <h2>AnimeApp</h2>
+
+        <div>
+          
+        </div>
+      </nav>
+    </header>
+  )
+}
